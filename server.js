9fa0740587c3799d432a880c685463b9c2d24394
Analin Flores <foglzerika@gmail.com>
2020-01-19 07:33:56 -0600

    Feature- create route and view for index page

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Hello world');
-})
+const index = require('./routes/index');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index);
 
 app.listen(process.env.PORT,()=>{
     console.log("Server is ready");
