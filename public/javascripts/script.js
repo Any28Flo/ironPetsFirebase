71e7bd710d7ca7028bc060f7bbfec017179055da
Analin Flores <foglzerika@gmail.com>
2020-01-19 15:36:44 -0600

    Adding firebase

---
+++ public/javascripts/script.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCzLmeFsDPH7IQ1gCHoMVtgGYiPSiGSx18",
+    authDomain: "ironpets-ffc95.firebaseapp.com",
+    databaseURL: "https://ironpets-ffc95.firebaseio.com",
+    projectId: "ironpets-ffc95",
+    storageBucket: "ironpets-ffc95.appspot.com",
+    messagingSenderId: "664017099282",
+    appId: "1:664017099282:web:ee84157fbd80f8077416cb",
+    measurementId: "G-HNV016DLTG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
--- server.js
+++ server.js
@@ -4,7 +4,29 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const app = express();
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
 
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzLmeFsDPH7IQ1gCHoMVtgGYiPSiGSx18",
+    authDomain: "ironpets-ffc95.firebaseapp.com",
+    databaseURL: "https://ironpets-ffc95.firebaseio.com",
+    projectId: "ironpets-ffc95",
+    storageBucket: "ironpets-ffc95.appspot.com",
+    messagingSenderId: "664017099282",
+    appId: "1:664017099282:web:ee84157fbd80f8077416cb",
+    measurementId: "G-HNV016DLTG"
+};
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firabese.database();
+  
 const index = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
